19a138a631931a1ef65a1dedec922d7e08063a94
luka <enanoodiaz2018@gmail.com>
2022-12-04 18:56:49 -0300

    pedir y mostrar productos

--- utilidades.js
+++ utilidades.js
@@ -1,12 +1,39 @@
-function pedirDatos(){
+function pedirUsuarios(){
   fetch ("https://json.extendsclass.com/bin/c43a83971051", {
       headers: {
         'Api-key': '9b6f0bba-6e8b-11ed-8b32-0242ac110002'
       }
   })
-  .then(response => response.text())
+  .then(response => response.json())
   .then(data =>{
-    let usuarios = JSON.parse(data).users;
-    document.getElementById("objeto1").innerHTML = usuarios[0].name;
+    let usuarios = data.users;
+    let producto;
+    for(let i = 0; i < usuarios.length; i = i + 1){
+      producto = document.createElement("div");
+      producto.className = "usuarios";
+      producto.innerHTML = "usuario: " + usuarios[i].name + "<br/>" + "mail: " + usuarios[i].email;
+      document.getElementById("usuarios").appendChild(producto)
+    }
+  });
+}
+
+function pedirProductos(){
+  fetch ("https://json.extendsclass.com/bin/c5e2a7b6e668", {
+      headers: {
+        'Api-key': '9b6f0bba-6e8b-11ed-8b32-0242ac110002'
+      }
+  })
+  .then(response => response.json())
+  .then(data =>{
+    let productos = data.productos;
+    let producto;
+    for(let i = 0; i < productos.length; i = i + 1){
+      producto = document.createElement("div");
+      producto.className = "productos";
+      producto.innerHTML =
+      '<img class="cuadradoImagen" src=' + productos[i].imagen + '></img><br/>'
+      + productos[i].descripcion;
+      document.getElementById("productos").appendChild(producto)
+    }
   });
 }
