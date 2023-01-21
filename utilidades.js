d46f938991ebb1f1e6fbb37e3e2498704fc38f70
luka <enanoodiaz2018@gmail.com>
2023-01-21 20:25:52 -0300

    hice el sistema de verificación de contraseñas

--- utilidades.js
+++ utilidades.js
@@ -1,4 +1,4 @@
-function pedirUsuarios(){
+function pedirUsuarios(email, password){
   fetch ("https://json.extendsclass.com/bin/c43a83971051", {
       headers: {
         'Api-key': '9b6f0bba-6e8b-11ed-8b32-0242ac110002'
@@ -7,13 +7,23 @@ function pedirUsuarios(){
   .then(response => response.json())
   .then(data =>{
     let usuarios = data.users;
-    let producto;
-    for(let i = 0; i < usuarios.length; i = i + 1){
-      producto = document.createElement("div");
-      producto.className = "usuarios";
-      producto.innerHTML = "usuario: " + usuarios[i].name + "<br/>" + "mail: " + usuarios[i].email;
-      document.getElementById("usuarios").appendChild(producto)
+    for(let i = 0; i < usuarios.length; i = i + 1) {
+      if(usuarios[i].email == email) {
+        console.log("email encontrado", usuarios[i]);
+        if (usuarios[i].password == password) {
+          cerrarModalLogueo()
+          return;
+        } else {
+          const error = document.createElement("span");
+          error.style.color = "red";
+          error.innerHTML = "contraseña incorrecta";
+          document.getElementById("div-logueo").appendChild(error);
+        }
+
+      }
     }
+    
+    console.log("email no encontrado")
   });
 }
 
@@ -38,9 +48,9 @@ function pedirProductos(){
     }
   });
 }
-function abrirModalRegistro() {
-  document.getElementById("registro").style.display = "flex";
+function abrirModalLogueo() {
+  document.getElementById("logueo").style.display = "flex";
 }
-function cerrarModalRegistro() {
-  document.getElementById("registro").style.display = "none";
+function cerrarModalLogueo() {
+  document.getElementById("logueo").style.display = "none";
 }
